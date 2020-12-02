557379dba22b9c9ad497478bcf19d97c7bb1c07a
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-12-02 11:30:05 +0100

    Created first step

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import './App.css';
 
+// VERVANG DEZE API KEY DOOR JOUW API KEY:
+const apiKey = 'd85e6256cb39361faf2b9dacc4eb33f4';
+
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchData() {
+    try {
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+      setWeatherData(result.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <>
       <div className="weather-container">
@@ -14,9 +29,17 @@ function App() {
           <SearchBar/>
 
           <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h1>14 &deg;</h1>
-            <button type="button">
+            {weatherData &&
+              <>
+                <h2>{weatherData.weather[0].description}</h2>
+                <h3>{weatherData.name}</h3>
+                <h1>{weatherData.main.temp}</h1>
+              </>
+            }
+            <button
+              type="button"
+              onClick={fetchData}
+            >
               Haal data op!
             </button>
           </span>
